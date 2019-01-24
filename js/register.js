0e12a410e1b3f9f67037232b37228d32b2cdca18
王雷 <honey@wangleideMacBook-Pro.local>
2019-01-24 19:55:56 +0800

    倒计时实现

--- js/register.js
+++ js/register.js
@@ -1,17 +1,45 @@
 mui.init({
-	
+
 });
 mui.plusReady(function() {
-	var yzmBtn = document.getElementById('yzm')
-	var nextOperate = document.getElementById('nextoperate')
+	var clock = '';
+	var nums = 60;
+	var yzmBtn = document.getElementById('yzm') //验证码
+	var phoneInput = document.getElementById('inpt_mobile') //手机号
+	var nextOperate = document.getElementById('nextoperate') //下一步
+	//发送验证码
 	yzmBtn.addEventListener('tap', function() {
-		console.log('点击验证码');
-		// 点击修改按钮内容
-		yzmBtn.innerHTML = '60秒';
-		// 点击以后按钮变成禁用状态
-		yzmBtn.disabled = true;
+		console.log('手机号', phoneInput.value)
+		if (phoneInput.value.trim() != "") {
+			// 点击以后按钮变成禁用状态
+			yzmBtn.disabled = true;
+			// 点击修改按钮内容
+			yzmBtn.innerHTML = nums + '秒';
+			clock = setInterval(doLoop, 1000)
+		} else {
+
+		}
+
+
 	})
-	nextOperate.addEventListener('tap',function(){
+	//倒计时
+	function doLoop() {
+		nums-- -
+		// console.log("开始")
+		if (nums > 0) {
+			yzmBtn.innerHTML = nums + '秒';
+		} else {
+			clearInterval(clock) //清除定时器
+			yzmBtn.disabled = false
+			yzmBtn.innerHTML = "发送验证码"
+			nums = 60
+			console.log("2")
+		}
+
+	}
+
+
+	nextOperate.addEventListener('tap', function() {
 		plus.nativeUI.closeWaiting();
 		mui.openWindow({
 			url: "registerBusiness.html"
