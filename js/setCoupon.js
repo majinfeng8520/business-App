4ae62f2f426bb599d7de4b49a8962d3a9a224bbe
majinfeng8520 <97102402@qq.com>
2019-02-08 17:01:18 +0800

    更新优惠券创建静态页面

---
+++ js/setCoupon.js
@@ -0,0 +1,12 @@
+mui.init({
+	
+})
+mui.plusReady(function () {
+    var oCoupon = document.getElementById('coupon');
+	var oIndex = oCoupon.selectedIndex;
+	var oAllowance = document.getElementById('allowance');
+	var oFull = document.getElementById('full');
+	var oExperience = document.getElementById('experience');
+	var oPresent = document.getElementById('present');
+	alert(1);
+})
